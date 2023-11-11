1c61f3e608a047090183aa198dc802db2a0437da
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-10 18:41:20 -0600

    Task 1

---
+++ web_flask/1-hbnb_route.py.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Returning Hello HBNB! & HBNB with using Flask"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    return 'Hello HBNB!'
+
+@app.route('/', strict_slashes=False)
+def HBNB_route():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
