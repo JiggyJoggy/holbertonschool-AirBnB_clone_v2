3505bf302b9782e7836731dc14a015a94d1a3196
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-10 20:19:25 -0600

    HTML Task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,8 +4,10 @@ display "C", followed by the value of the text variable
 display "Python", followed by the value of the text variable
 The default value of text is “is cool”
 display “n is a number” only if n is an integer
+display a HTML page only if n is an integer
 """
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
@@ -36,6 +38,10 @@ def python_(text=None):
 def number(n):
     return f'{n} is a number'
 
+
+@app.route('number_template/<int:n>', strict_slashes=False)
+def template_number(n):
+    return render_template('5-number.html', number=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
