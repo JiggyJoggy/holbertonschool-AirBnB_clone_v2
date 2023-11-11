c17160523e5f24ec4b6ef89ace9a5029aef55232
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-10 20:38:18 -0600

    i forgot a slash

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""Using Flask for Web"""
+"""Returning Hello HBNB! & HBNB with using Flask
+display "C", followed by the value of the text variable
+display "Python", followed by the value of the text variable
+The default value of text is “is cool”
+display “n is a number” only if n is an integer
+display a HTML page only if n is an integer"""
 from flask import Flask
 from flask import render_template
 
@@ -33,7 +38,7 @@ def number(n):
     return f'{n} is a number'
 
 
-@app.route('number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def template_number(n):
     return render_template('5-number.html', number=n)
 
