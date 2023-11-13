b62a788d5d4f398b02c8e971f851978c233f6197
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-13 14:15:30 -0600

    test

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    
+
     cities = relationship("City", backref="state", cascade="all, delete")
 
     @property
