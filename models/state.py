44bc8098a5999a92c45236baa8973109139286bb
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-13 14:06:57 -0600

    test

--- models/state.py
+++ models/state.py
@@ -12,16 +12,14 @@ class State(BaseModel):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship(
-            "City", backref="state", cascade="all, delete"
-        )
-    else:
-        @property
-        def cities(self):
-            """Returns list of cities"""
-            cities_list = []
-            for obj in list(models.storage.all(City).values()):
-                if obj.state_id == self.id:
-                    cities_list.append(obj)
-                return cities_list
+    
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        from models import storage
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
