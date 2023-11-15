82732228c0cfd68909ea3854218fd79af017af0c
Jagger Van Winkle <JaggerVanW@gmail.com>
2023-11-14 22:45:46 -0600

    test

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""
-initialize the models package
-"""
-
+"""This module instantiates an object of class FileStorage"""
 from os import getenv
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
 
 
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
